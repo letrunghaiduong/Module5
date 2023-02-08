9e4284b26671265cd245866c4b66ac27037ca9af
letrunghaiduong <letrunghaiduong260701@gmail.com>
2023-02-08 20:02:01 +0700

    update

--- ss6/exercise/form-login/src/app/form-login/form-login.component.ts
+++ ss6/exercise/form-login/src/app/form-login/form-login.component.ts
@@ -13,30 +13,22 @@ export class FormLoginComponent implements OnInit {
     this.formLogin = new FormGroup({
       email: new FormControl("",[Validators.required,Validators.email]),
       password: new FormControl("",[Validators.required,Validators.minLength(6)]),
-      confirmPassword: new FormControl("",[Validators.required,Validators.minLength(6)]),
+      confirmPassword: new FormControl("",[this.checkPasswords,Validators.required]),
       country: new FormControl("",[Validators.required]),
       age: new FormControl("",[Validators.required,Validators.min(18)]),
       gender: new FormControl("",[Validators.required]),
-      phone: new FormControl("",[Validators.required])
-    },{
-      validators: this.checkPassword(`password`, `confirmPassword`)
+      phone: new FormControl("",[Validators.required,Validators.pattern('^(0[3|5|7|8|9])+([0-9]{8})$')])
     });
   }
 
-  checkPassword(controlPass: string, matchingControlConfirm: string): any {
-    return (formGroup: FormGroup) => {
-      const control = formGroup.controls[controlPass];
-      const matching = formGroup.controls[matchingControlConfirm];
-      if (matching.errors && !matching.errors.checkPassword) {
-        return;
-      }
-      if (control.value != matching.value) {
-        return matching.setErrors({checkPassword: true});
-      } else {
-        return matching.setErrors(null);
-      }
-    };
+  checkPasswords(group: AbstractControl){
+    let checkPassword = group.value;
+    if(checkPassword.password === checkPassword.confirmPassword){
+      return null;
+    }
+    return {'notSame': true};
   }
+
 
   ngOnInit(): void {
   }
