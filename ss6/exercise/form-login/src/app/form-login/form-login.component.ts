61036a6172a0d4c7160d95edb1ec37b3a56a8429
letrunghaiduong <letrunghaiduong260701@gmail.com>
2023-02-09 20:21:45 +0700

    update

--- ss6/exercise/form-login/src/app/form-login/form-login.component.ts
+++ ss6/exercise/form-login/src/app/form-login/form-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
@@ -8,22 +8,27 @@ import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/form
 })
 export class FormLoginComponent implements OnInit {
   formLogin: FormGroup;
+  countryList = [
+    {id: 1, name: 'Đà Nẵng'},
+    {id: 2, name: 'Hà Nội'},
+    {id: 3, name: 'TP.Hồ Chí Minh'}
+  ];
 
   constructor() {
     this.formLogin = new FormGroup({
-      email: new FormControl("",[Validators.required,Validators.email]),
-      password: new FormControl("",[Validators.required,Validators.minLength(6)]),
-      confirmPassword: new FormControl("",[this.checkPasswords,Validators.required]),
-      country: new FormControl("",[Validators.required]),
-      age: new FormControl("",[Validators.required,Validators.min(18)]),
-      gender: new FormControl("",[Validators.required]),
-      phone: new FormControl("",[Validators.required,Validators.pattern('^(0[3|5|7|8|9])+([0-9]{8})$')])
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', [this.checkPasswords, Validators.required]),
+      country: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required, Validators.min(18)]),
+      gender: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required, Validators.pattern('^(0[3|5|7|8|9])+([0-9]{8})$')])
     });
   }
 
-  checkPasswords(group: AbstractControl){
+  checkPasswords(group: AbstractControl) {
     let checkPassword = group.value;
-    if(checkPassword.password === checkPassword.confirmPassword){
+    if (checkPassword.password === checkPassword.confirmPassword) {
       return null;
     }
     return {'notSame': true};
